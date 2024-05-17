5a96e750bdec050e1a5ca0a723191557eee03ee8
AbyssClient <iloaf9820@gmail.com>
2024-05-17 13:50:43 +0100

    Mixin client files

---
+++ src/main/java/net/example/exampleclient/mixins/mixin/client/MixinMinecraft.java
@@ -0,0 +1,16 @@
+package net.example.exampleclient.mixins.mixin.client;
+
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Minecraft.class)
+public class MixinMinecraft {
+
+    @Inject(method = "startGame", at = @At("HEAD"))
+    private void startGame(CallbackInfo ci) {
+        System.out.println("Example client has started");
+    }
+}
